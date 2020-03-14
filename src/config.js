380fc9933c23f91a965d949f00d49a1e51b62402
cdnTwark <terry.warkentin@gmail.com>
2020-03-14 14:16:46 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "twark-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://szew8w0ot2.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_hbWkZdNx3",
+      APP_CLIENT_ID: "a5814ba111relh42016a1suo8",
+      IDENTITY_POOL_ID: "us-east-1:da2abdb4-b761-4df6-a3b1-827671fc6589"
+    }
+  };
