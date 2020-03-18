5bdd33be858462123c6d18e4b7fc94b803771d52
cdnTwark <terry.warkentin@gmail.com>
2020-03-18 14:43:13 -0700

    Building our React App

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_nt8Dc6WskZlSUyuWIbyvwVqP00pYGsQPI8",
     s3: {
       REGION: "us-east-1",
       BUCKET: "twark-app-uploads"
